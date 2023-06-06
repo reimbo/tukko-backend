73231628840ff073272401a8ae8798d6990fe248
Iusbus <hanninenjustus@gmail.com>
2023-06-06 12:26:58 +0300

    Connect to mongoDB

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express } from 'express';
 import { tmsRouter } from './routes/tms_data'; 
+import { connect } from './scripts/mongo';
+
 
 const app: Express = express()
 app.use(express.json())
 const port: Number = parseInt(process.env.PORT as string) || 3000
 
-app.use("/tms", tmsRouter)
+connect()
+  .then((): void => {
+    app.use("/tms", tmsRouter)
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-})
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`)
+    })
+  })
+  .catch((error: Error): void => {
+    console.error("Database connection failed", error);
+    process.exit()
+  })
