540b25c1c48711dae80b0c983ad8569ef3503f7f
AA7776 <aa7776@student.jamk.fi>
2023-06-14 08:44:24 +0300

    create aggregated sort for a specific sensor data

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import express, { Express } from 'express';
 import { tmsRouter } from './routes/tms_data'; 
-import { connect } from './scripts/mongo';
+import { connect} from './scripts/mongo';
+import { fetch } from "./scripts/fetch";
 
 const app: Express = express();
 app.use(express.json());
-const port: number = parseInt(process.env.PORT as string) || 3001;
+const port: number = parseInt(process.env.PORT as string);
 
 connect()
   .then((): void => {
@@ -14,11 +15,10 @@ connect()
       console.log(`Server running on port ${port}`);
     });
 
-    
   })
   .catch((error: Error): void => {
     console.error("Database connection failed", error);
     process.exit();
   });
-
+fetch("https://tie.digitraffic.fi/api/tms/v1/stations/data")
   
--- src/scripts/fetch.ts
+++ src/scripts/fetch.ts
@@ -1,7 +1,7 @@
 const axios = require('axios').default;
 import * as mongoDB from "mongodb"
-import { collections } from "../scripts/mongo";
-import { StationData, Station, Sensor } from "models/tms_data_model";
+import { collections} from "../scripts/mongo";
+import {  Station, Sensor } from "models/tms_data_model";
 import { AxiosResponse } from "axios";
 
 const axiosConf = {
@@ -14,6 +14,7 @@ export const fetch = async (url: String): Promise<mongoDB.Document | unknown> =>
   try {
     const response: AxiosResponse = await axios.get(url, axiosConf)
     const data = await storeFetch(response.data)
+    await runAggregation()
     return data
   } catch (error) {
     console.log(error);
@@ -23,6 +24,21 @@ export const fetch = async (url: String): Promise<mongoDB.Document | unknown> =>
 
 const storeFetch = async (data: any): Promise<mongoDB.InsertOneResult<mongoDB.Document> | unknown>  => {
   try {
+    // const existingData = await getStore("23172") as mongoDB.Document | undefined;
+    // console.log(existingData && existingData[0].dataUpdatedTime instanceof Date ? existingData[0].dataUpdatedTime.toDateString() : undefined);
+    // console.log(existingData)
+    // if (existingData && existingData.dataUpdatedTime instanceof Date) {
+    //   const currentDate = new Date();
+
+    //   if (
+    //     existingData.dataUpdatedTime.getDate() === currentDate.getDate() &&
+    //     existingData.dataUpdatedTime.getMonth() === currentDate.getMonth() &&
+    //     existingData.dataUpdatedTime.getFullYear() === currentDate.getFullYear()
+    //   ) {
+    //     console.log('Data is already up to date. Skipping update.');
+    //     return existingData;
+    //   }
+    // }
     const stations: Station[] = data.stations.map((station: Station) => {
       const sensorValues: Sensor[] = station.sensorValues.map((sensor: Sensor) => ({
           id: sensor.id,
@@ -39,16 +55,23 @@ const storeFetch = async (data: any): Promise<mongoDB.InsertOneResult<mongoDB.Do
       return {
         id: station.id,
         tmsNumber: station.tmsNumber,
-        dataUpdatedTime: station.dataUpdatedTime,
+        dataUpdatedTime: new Date(station.dataUpdatedTime),
         sensorValues: sensorValues
       }
     })
-    
-    const result = await collections.tms?.insertOne({
-      dataUpdatedTime: data.dataUpdatedTime,
-      stations: stations,
-    });
-
+    // console.log(typeof(stations[0].dataUpdatedTime))
+    // console.log(data.dataUpdatedTime)
+    // console.log(stations.length)
+    // const result = await collections.tms?.updateOne(
+    //   { dataUpdatedTime: data.dataUpdatedTime },
+    //   { $setOnInsert: { stations: stations } },
+    //   { upsert: true }
+    // );
+    const result = await collections.tms?.updateOne(
+      {},
+      { $set: { stations: stations }, $setOnInsert: { dataUpdatedTime: data.dataUpdatedTime } },
+      { upsert: true }
+    );
     if (!result) {
       throw new Error('Failed to insert data into MongoDB');
       }
@@ -61,13 +84,111 @@ const storeFetch = async (data: any): Promise<mongoDB.InsertOneResult<mongoDB.Do
   }
 }
 
-const getStore = async (): Promise<mongoDB.Document | unknown >=> {
+const getStore = async (
+  stationId?: string,
+  sensorId?: string,
+  sensorName?: string,
+  date?: Date
+  ): Promise<mongoDB.Document | undefined> => {
   try {
-    const stationDoc = (await collections.tms?.findOne({})) as mongoDB.Document
-    console.log(typeof(stationDoc))
-    return stationDoc
+    // const query: any = {};
+
+    const criteria: any[] = [];
+
+    if (stationId) {
+      // console.log(stationId)
+      criteria.push({ 'stations.id': stationId });
+    }
+
+    if (sensorId) {
+      console.log(sensorId+"sensor***")
+      criteria.push({ 'stations.sensorValues.id': sensorId });
+    }
+
+    if (sensorName) {
+      criteria.push({ 'stations.sensorValues.name': sensorName });
+    }
+
+    if (date) {
+      criteria.push({ 'stations.dataUpdatedTime': { $gte: date } });
+    }
+    
+    const query: any = criteria.length > 0 ? { $and: criteria } : {};
+    if (criteria.length > 0) {
+      query.$and = criteria;
+      // console.log(query)
+    }
+
+    const stationDoc = await collections.tms?.findOne(query);
+    return stationDoc ?? undefined;
+  } catch (error: unknown) {
+    console.error(error);
+    return undefined;
+  }
+};
+
+//db.tms.find( { stations: { $elemMatch: { tmsNumber: 20002, 'sensorValues.stationId':20002 } }})
+//  db.tms.find( { stations: { $elemMatch: { tmsNumber: 130, 'sensorValues.id':5158 } }})
+// db.tms.find( { }, {stations: 0}) // will not show stations
+// db.tms.find( { }, {'stations.sensorValues': 1}) // only show sensorValues
+//// Assuming you have a collection called 'tms' and each document contains 'stations' array with 'sensorValues' objects
+
+// To show only selected sensor objects with a specific sensorID from each sensorValues array:
+// db.tms.aggregate([ { $match: { 'stations.sensorValues.name': 'OHITUKSET_5MIN_LIUKUVA_SUUNTA2_MS2' } }, { $project: { stations: { $map: { input: '$stations', as: 'station', in: { sensorValues: { $filter: { input: '$$station.sensorValues', as: 'sensor', cond: { $eq: ['$$sensor.name', 'OHITUKSET_5MIN_LIUKUVA_SUUNTA2_MS2'] } } } } } } } }])
+// db.tms.aggregate([{ $match: { 'stations.sensorValues.name': 'OHITUKSET_5MIN_LIUKUVA_SUUNTA2_MS2' } }, { $project: { stations: { $map: { input: '$stations', as: 'station', in: { sensorValues: { $filter: { input: '$$station.sensorValues', as: 'sensor', cond: { $eq: ['$$sensor.name', 'OHITUKSET_5MIN_LIUKUVA_SUUNTA2_MS2'] } } } } } } } }, { $unwind: '$stations' }, { $unwind: '$stations.sensorValues' }, { $sort: { 'stations.sensorValues.value': -1 } }])
+
+export async function runAggregation() {
+
+  try {
+
+    const aggregationPipeline = [
+      {
+        $match: {
+          'stations.sensorValues.name': 'OHITUKSET_5MIN_LIUKUVA_SUUNTA2_MS2'
+        }
+      },
+      {
+        $project: {
+          stations: {
+            $map: {
+              input: '$stations',
+              as: 'station',
+              in: {
+                sensorValues: {
+                  $filter: {
+                    input: '$$station.sensorValues',
+                    as: 'sensor',
+                    cond: {
+                      $eq: ['$$sensor.name', 'OHITUKSET_5MIN_LIUKUVA_SUUNTA2_MS2']
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      {
+        $unwind: '$stations'
+      },
+      {
+        $unwind: '$stations.sensorValues'
+      },
+      {
+        $sort: {
+          'stations.sensorValues.value': -1
+        }
+      }
+    ];
+
+    const result = await collections.tms?.aggregate(aggregationPipeline).toArray();
+    if(!result) {
+      throw new Error('Failed to aggregate data from MongoDB');
+    }
+    console.log(result[0].stations.sensorValues.value);
+    const filteredResult = result.map((station: any) => station.stations.sensorValues.value);
+    console.log(filteredResult);
   } catch (error: unknown) {
     console.error(error)
-    return error
   }
 }
