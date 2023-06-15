ffdb8580be466140cdf69538cda5e34a9c021543
AA7776 <aa7776@student.jamk.fi>
2023-06-15 09:01:59 +0300

    combined data for multiple responses

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,23 @@
 import express, { Express } from 'express';
 import { tmsRouter } from './routes/tms_data'; 
 import { connect} from './scripts/mongo';
-import { fetch } from "./scripts/fetch";
+import { fetch, runAggregation } from "./scripts/fetch";
 
 const app: Express = express();
 app.use(express.json());
 const port: number = parseInt(process.env.PORT as string);
 
+const searchString = "KESKINOPEUS_5MIN_LIUKUVA_SUUNTA2";
+
 connect()
-  .then((): void => {
+  .then(async (): Promise<void> => {
     app.use("/tms", tmsRouter);
 
     app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
+
+    // await runAggregation(searchString)
 
   })
   .catch((error: Error): void => {
@@ -21,4 +25,4 @@ connect()
     process.exit();
   });
 fetch("https://tie.digitraffic.fi/api/tms/v1/stations/data")
-  
+// fetch("https://tie.digitraffic.fi/api/tms/v1/stations")
