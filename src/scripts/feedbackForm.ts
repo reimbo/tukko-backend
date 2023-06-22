a72912d677553a9810edfc0890b82a0e2d553c7d
AA7776 <aa7776@student.jamk.fi>
2023-06-22 14:10:15 +0300

    added tms/feedback endpoint to receive and post feedback from front end

---
+++ src/scripts/feedbackForm.ts
@@ -0,0 +1,39 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+interface Inputs {
+    title?: string;
+    description?: string;
+}
+
+export const FeedbackForm = async (initialInputs: Inputs) => {
+    const url = "https://gitlab.labranet.jamk.fi/api/v4/projects/23409/issues";
+    const params = new URLSearchParams({
+        title: initialInputs.title || "",
+        description: initialInputs.description || "",
+        labels: "Customer Feedback",
+    });
+
+    const token = process.env.VITE_PROJECT_ACCESS_TOKEN as string;
+
+    const headers = {
+        "Private-Token": token,
+    };
+
+    try {
+        const response = await fetch(`${url}?${params}`, {
+        method: "POST",
+        headers: headers,
+        });
+
+        if (response.ok) {
+        return response;
+        } else {
+        const error = await response.json();
+        return error;
+        }
+    } catch (error: any) {
+        console.log(`Error: ${error.message}`);
+    }
+};
