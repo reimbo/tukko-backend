7d14a185ac0a3a21752bded5aaadf08206a2344b
AB0189 <ab0189@student.jamk.fi>
2023-07-11 16:39:01 +0300

    Removing sensor keys

--- src/routes/redis/queryValidation.ts
+++ src/routes/redis/queryValidation.ts
@@ -1,87 +1,88 @@
-import { StatusCodes } from 'http-status-codes';
-import { ParsedQs } from 'qs';
+import { StatusCodes } from "http-status-codes";
+import { ParsedQs } from "qs";
 
 // Dictionary to store expected data types for each parameter of station and sensor queries
 const stationParameterTypes: Record<string, string> = {
-    'longitude': 'number',
-    'latitude': 'number',
-    'radius': 'number',
-    'roadNumber': 'integer',
-    'roadSection': 'integer',
-    'municipalityCode': 'integer',
-    'provinceCode': 'integer',
-    'id': 'integer',
-    'stationId': 'integer',
-    'measuredTimeOnAfter': 'date-time',
-    'measuredTimeOnBefore': 'date-time',
-    'valueGte': 'number',
-    'valueLte': 'number'
+  collectionStatus: "collectionStatus",
+  longitude: "number",
+  latitude: "number",
+  radius: "number",
+  roadNumber: "integer",
+  roadSection: "integer",
+  municipalityCode: "integer",
+  provinceCode: "integer",
 };
 
 // Dictionary to store expected data types for each parameter of road work queries
 const roadworkParameterTypes: Record<string, string> = {
-    'primaryPointRoadNumber': 'integer',
-    'primaryPointRoadSection': 'integer',
-    'secondaryPointRoadNumber': 'integer',
-    'secondaryPointRoadSection': 'integer',
-    'startTimeOnAfter': 'date-time',
-    'startTimeOnBefore': 'date-time',
-    'severity': 'severity'
+  primaryPointRoadNumber: "integer",
+  primaryPointRoadSection: "integer",
+  secondaryPointRoadNumber: "integer",
+  secondaryPointRoadSection: "integer",
+  startTimeOnAfter: "date-time",
+  startTimeOnBefore: "date-time",
+  severity: "severity",
 };
 
 // Helper function to check if a value is of the expected data type
 function isValidType(value: string, expectedType: string) {
-    if (expectedType === 'boolean') {
-        return value === 'true' || value === 'false';
-    } else if (expectedType === 'string') {
-        return typeof value === 'string';
-    } else if (expectedType === 'severity') {
-        return (value === 'LOW' || value === 'HIGH' || value === 'HIGHEST');
-    } else if (expectedType === 'number') {
-        return !isNaN(Number(value));
-    } else if (expectedType === 'integer') {
-        return Number.isInteger(Number(value));
-    } else if (expectedType === 'date-time') {
-        const date = new Date(value);
-        return date instanceof Date && !isNaN(date.getTime());
-    }
-    return false;
+  if (expectedType === "boolean") {
+    return value === "true" || value === "false";
+  } else if (expectedType === "string") {
+    return typeof value === "string";
+  } else if (expectedType === "collectionStatus") {
+    return value === "GATHERING" || value === "REMOVED_TEMPORARILY" || value === "REMOVED_PERMANENTLY ";
+  } else if (expectedType === "severity") {
+    return value === "LOW" || value === "HIGH" || value === "HIGHEST";
+  } else if (expectedType === "number") {
+    return !isNaN(Number(value));
+  } else if (expectedType === "integer") {
+    return Number.isInteger(Number(value));
+  } else if (expectedType === "date-time") {
+    const date = new Date(value);
+    return date instanceof Date && !isNaN(date.getTime());
+  }
+  return false;
 }
 
 // Helper function to throw the 400 status code error
 function throwValidationError(param: string) {
-    const error: any = new Error(`Invalid value for parameter '${param}'.`);
-    error.error = 'Bad Request';
-    error.statusCode = StatusCodes.BAD_REQUEST;
-    throw error;
+  const error: any = new Error(`Invalid value for parameter '${param}'.`);
+  error.error = "Bad Request";
+  error.statusCode = StatusCodes.BAD_REQUEST;
+  throw error;
 }
 
 // Helper function to validate query params using passed parameter types
-function validateQueryParams(params: ParsedQs, parameterTypes: Record<string, any>) {
-    const keys = new Set<string>(Object.keys(params));
-    for (const param of keys) {
-        if (param in parameterTypes) {
-            // Protect against arrays
-            if (Array.isArray(params[param])) {
-                const values: any = params[param];
-                for (const value in values) {
-                    if (!isValidType(value as string, parameterTypes[param])) throwValidationError(value);
-                }
-            }
-            else {
-                const value = params[param] as string;
-                if (!isValidType(value, parameterTypes[param])) throwValidationError(value);
-            }
+function validateQueryParams(
+  params: ParsedQs,
+  parameterTypes: Record<string, any>
+) {
+  const keys = new Set<string>(Object.keys(params));
+  for (const param of keys) {
+    if (param in parameterTypes) {
+      // Protect against arrays
+      if (Array.isArray(params[param])) {
+        const values: any = params[param];
+        for (const value in values) {
+          if (!isValidType(value as string, parameterTypes[param]))
+            throwValidationError(param);
         }
+      } else {
+        const value = params[param] as string;
+        if (!isValidType(value, parameterTypes[param]))
+          throwValidationError(param);
+      }
     }
+  }
 }
 
 // Function to validate station query parameter types
 export function validateStationQueryParams(params: ParsedQs) {
-    validateQueryParams(params, stationParameterTypes);
+  validateQueryParams(params, stationParameterTypes);
 }
 
 // Function to validate roadwork query parameter types
 export function validateRoadworkQueryParams(params: ParsedQs) {
-    validateQueryParams(params, roadworkParameterTypes);
+  validateQueryParams(params, roadworkParameterTypes);
 }
--- src/scripts/schedule.ts
+++ src/scripts/schedule.ts
@@ -1,17 +1,23 @@
 // Delay in milliseconds
 export async function delayBy(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 // Schedule a script with rate in milliseconds
-export async function scheduleScript(script: () => Promise<void>, startDelay: number, rate: number) {
-    await delayBy(startDelay);
-    while (true) {
-        // Run the script
-        console.log(`Running ${script.name} script with a rate of ${rate / 1000} seconds...`);
-        await script();
-        console.log(`Finished running ${script.name} script.`);
-        // Wait for a specific delay using a Promise and setTimeout
-        await delayBy(rate);
-    }
+export async function scheduleScript(
+  script: () => Promise<void>,
+  startDelay: number,
+  rate: number
+) {
+  await delayBy(startDelay);
+  while (true) {
+    // Run the script
+    console.log(
+      `Running ${script.name} script with a rate of ${rate / 1000} seconds...`
+    );
+    await script();
+    console.log(`Finished running ${script.name} script.`);
+    // Wait for a specific delay using a Promise and setTimeout
+    await delayBy(rate);
+  }
 }
